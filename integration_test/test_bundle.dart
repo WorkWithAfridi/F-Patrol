f60ceb9037e4f5698ae75a4def919b7b04ee2d1a
Khondakar Afridi <afridi.khondakar@gmail.com>
2025-03-05 01:14:06 +0600

    Added ios configuration

--- integration_test/test_bundle.dart
+++ integration_test/test_bundle.dart
@@ -9,7 +9,7 @@ import 'package:patrol/src/native/contracts/contracts.dart';
 import 'package:test_api/src/backend/invoker.dart';
 
 // START: GENERATED TEST IMPORTS
-import 'app_flow_test.dart' as app_flow_test;
+import 'example_test.dart' as example_test;
 // END: GENERATED TEST IMPORTS
 
 Future<void> main() async {
@@ -69,7 +69,7 @@ Future<void> main() async {
   });
 
   // START: GENERATED TEST GROUPS
-  group('app_flow_test', app_flow_test.main);
+  group('example_test', example_test.main);
   // END: GENERATED TEST GROUPS
 
   final dartTestGroup = await testExplorationCompleter.future;
